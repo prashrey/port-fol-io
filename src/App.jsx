c0176737dd030915c30490b31a69a42c87cc53ce
Prashrey <prashrey04@gmail.com>
2025-07-13 21:14:52 +0530

    updated latest code

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,10 @@ function App() {
       <Header />
       <main className="main-wrap">
         <Profile />
-        {/* <section id="experience" className="section">
-          <h2>Experience</h2>
-        </section>
-        <section id="projects" className="section">
-          <h2>Projects</h2>
-        </section>
-        <section id="summary" className="section">
-          <h2>Summary</h2>
-        </section> */}
         <ResumeContent />
       </main>
       <footer className="footer">
-        <p>&copy; 2025 All rights reserved.</p>
+        <p>&copy; 2025 All 0 rights reserved.</p>
       </footer>
     </>
   );
